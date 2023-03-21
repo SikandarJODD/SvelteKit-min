c77aca21e1fa9ff0aa834b5f700ff9d014badaff
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-03-21 20:08:54 +0530

    Added Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+  <h1 class="text-4xl font-bold ">
+    Welcome to <span class="text-blue-500">Tailwind + Sveltekit</span> Application
+  </h1>
+  <div class="flex flex-col text-center">
+    <img
+      src="https://media.licdn.com/dms/image/C4D03AQGP1dNhTLkZrA/profile-displayphoto-shrink_400_400/0/1662137583464?e=1684972800&v=beta&t=T1u2k6poM941aT_YRKyXV6Km2Z3rwMlD_mppIxgUlM4"
+      alt="Sikandar.S.Bhide"
+      class="rounded-full h-52 w-52 mt-10 ring-2 ring-blue-600 shadow-img "
+    />
+    <a
+      href="https://www.linkedin.com/in/sikandar-bhide-911988211/"
+      class="mt-2 font-medium text-sky-600 font-mono"
+      target="_blank"
+    >
+      <h3 class="text-2xl">Sikandar.S.Bhide</h3>
+    </a>
+  </div>
+</section>
+
+<style lang="postcss">
+  /* :global(html) {
+    background-color: theme(colors.gray.100);
+  } */
+  section {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 30px;
+  }
+</style>
